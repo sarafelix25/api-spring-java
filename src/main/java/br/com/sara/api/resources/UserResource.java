d311d96ca4f12fd3edd3bf7e38077fd342031ca4
sarafelix_ <sarabfelix1@gmail.com>
2024-08-20 17:13:28 -0300

    create user resouce

---
+++ src/main/java/br/com/sara/api/resources/UserResource.java
@@ -0,0 +1,18 @@
+package br.com.sara.api.resources;
+
+import br.com.sara.api.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(new User(2, "Batista", "email@email.com", "12345678"));
+    }
+}
