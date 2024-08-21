14f2598e0b4894a62c6468994738fc051345620e
sarafelix_ <sarabfelix1@gmail.com>
2024-08-21 11:37:36 -0300

    create class UserDto

--- src/main/java/br/com/sara/api/dto/UserDto.java
+++ src/main/java/br/com/sara/api/dto/UserDto.java
@@ -1,2 +1,19 @@
-package br.com.sara.api.dto;public class UserDto {
+package br.com.sara.api.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDto {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
 }
